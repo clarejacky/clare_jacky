876186885fef4cc60879d294ee3f1375159a4493
clarejacky <clare.jacky@gmail.com>
2015-05-07 15:30:08 -0500

    get request

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,7 +1,11 @@
+var htmldata;
+
 $(document).ready(fucntion(){
     $(".resume").on("click", function(){
 
-        $.get('resume.html', function (data) {
+        $.get('views/resume.html', function (data) {
+            htmldata = data
+            (".resumeDiv").append(htmldata);
 
         });
 
