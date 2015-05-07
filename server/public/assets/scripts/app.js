68b6544b9c4d17ffcd79ff8737958bf574ef6eeb
clarejacky <clare.jacky@gmail.com>
2015-05-07 16:49:21 -0500

    semi colon

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $(".resume").on("click", function(){
 
         $.get('views/resume.html', function (data) {
-            htmldata = data
+            htmldata = data;
             (".resumeDiv").append(htmldata);
 
         });
